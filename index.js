01e8bb5ddb7d3d35a4c2f2d8fbc262e40ec3b46f
Rasmus Järnström <rasmus.jarnstrom@aalto.fi>
2017-11-14 10:05:39 +0200

    Added node. Created boilerplate index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.set('view engine', 'ejs');
+
+//Main request for index site
+app.get('/', function(request, response) {
+  response.render('pages/index');
+});
+
+//Post request
+app.post('/api/post', function(request, response) {
+  response.send('Save succeeded.');
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});

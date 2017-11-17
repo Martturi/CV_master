b509ed080a9aad08fde33dfa192fae8d613d3d6b
Rasmus Järnström <rasmus.jarnstrom@aalto.fi>
2017-11-17 23:41:05 +0200

    Test changes

--- index.js
+++ index.js
@@ -5,26 +5,27 @@ var bodyParser = require("body-parser");
 var app = express();
 const { Client } = require('pg');
 
+app.set('port', (process.env.PORT || 5000));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+var text = ''
+
+
 const client = new Client({
   connectionString: process.env.DATABASE_URL,
   ssl: true,
 });
-
-var text = ''
 
 client.connect();
 
 client.query('SELECT text FROM CVs;', (err, res) => {
   if (err) throw err;
   for (let row of res.rows) {
-    text = row
+    console.log("row:" + row);
   }
   client.end();
 });
-
-app.set('port', (process.env.PORT || 5000));
-app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({ extended: true }));
 
 //Main request for index site
 app.get('/', function(request, response) {

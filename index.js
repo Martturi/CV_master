e422f776ca9e5c5354e2f9fc32b5aa958baa27a3
Rasmus Järnström <rasmus.jarnstrom@aalto.fi>
2017-11-17 23:30:29 +0200

    Updates text from DB

--- index.js
+++ index.js
@@ -3,6 +3,24 @@ var fs = require('fs');
 var path = require("path");
 var bodyParser = require("body-parser");
 var app = express();
+const { Client } = require('pg');
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+});
+
+var text = ''
+
+client.connect();
+
+client.query('SELECT text FROM CVs;', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    text = row
+  }
+  client.end();
+});
 
 app.set('port', (process.env.PORT || 5000));
 app.set('view engine', 'ejs');
@@ -12,8 +30,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.get('/', function(request, response) {
   response.sendFile(path.join(__dirname+'/CV.html'));
 });
-
-var text = ''
 
 //Post request, saves text
 app.post('/api/post', function(request, response) {

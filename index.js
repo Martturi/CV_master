441ae9b6f1d7d36e3f02cc77a94f1ec13d486aa8
Rasmus Järnström <rasmus.jarnstrom@aalto.fi>
2017-11-20 10:48:53 +0200

    Edited DB install instructions

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const client = new Client({
 
 client.connect();
 
-client.query('SELECT text FROM CVs WHERE id = 0;', (err, res) => {
+client.query('SELECT text FROM cv_table WHERE id = 0;', (err, res) => {
   if (err) throw err;
     JSON.parse(JSON.stringify(res.rows[0]), (key, value) => {
       if (key == "text") {
@@ -31,7 +31,7 @@ client.query('SELECT text FROM CVs WHERE id = 0;', (err, res) => {
 function saveCV(text) {
   client.connect();
 
-  client.query('UPDATE cvs SET text='+text+' WHERE id = 0;', (err, res) => {
+  client.query('UPDATE cv_table SET text='+text+' WHERE id = 0;', (err, res) => {
     if (err) throw err;
     client.end();
   });

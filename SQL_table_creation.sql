f01afb98a1eefbe6fd270bf68a30af2e0d1f78dd
Rasmus Järnström <rasmus.jarnstrom@aalto.fi>
2017-11-25 11:23:49 +0200

    Small changes

--- SQL_table_creation.sql
+++ SQL_table_creation.sql
@@ -1,6 +1,8 @@
-CREATE TABLE   "public"."cv_table" (  
-      "id" serial,  
-      "text" text,  
-      PRIMARY KEY ("id")  
-    );  
-    INSERT INTO   "public"."cv_table"("id", "text")   VALUES(0, 'DEFAULT CV') RETURNING "id", "text";
+CREATE DATABASE CV_DB;
+
+CREATE TABLE   "public"."cv_table" (
+      "id" serial,
+      "text" text,
+      PRIMARY KEY ("id")
+);
+INSERT INTO   "public"."cv_table"("id", "text")   VALUES(0, 'DEFAULT CV') RETURNING "id", "text";

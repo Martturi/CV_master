000711e91acf49094363ee63828c23966bb64216
Rasmus Järnström <rasmus.jarnstrom@aalto.fi>
2017-11-27 10:27:56 +0200

    Fixed esLint errors

--- src/routes.js
+++ src/routes.js
@@ -1,34 +1,35 @@
-var express = require('express');
-var route = express();
-var path = require("path");
-var bodyParser = require("body-parser");
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const db = require('./db');
 
-var db = require('./db');
+const route = express();
+
 
 route.set('port', (process.env.PORT || 5000));
-//route.set('views', __dirname + '/views');
+// route.set('views', __dirname + '/views');
 route.set('view engine', 'ejs');
 
 route.use(bodyParser.urlencoded({ extended: true }));
 
-//Main request for index site
+// Main request for index site
 route.get('/', (request, response) => {
-  response.sendFile(path.join(__dirname + '/views/CV.html'));
+  response.sendFile(path.join(__dirname, '/views/CV.html'));
 });
 
-//Post request, saves text
+// Post request, saves text
 route.post('/api/post', (request, response) => {
-  var input = request.body.textfield || null;
+  const input = request.body.textfield || null;
   response.send(db.save(input));
 });
 
-//Get request
+// Get request
 route.get('/api/get', (request, response) => {
   response.send(db.load());
 });
 
 route.listen(route.get('port'), () => {
-  console.log('Node app is running on port', route.get('port'));
+  // console.log('Node app is running on port', route.get('port'));
 });
 
 module.exports = route;

6d8008268ed7f832cccc965047c0d370b91e39ba
samipusa <sami.pusa@gmail.com>
2017-12-07 19:31:13 +0200

    added more tables

--- SQL_table_creation.sql
+++ SQL_table_creation.sql
@@ -5,14 +5,13 @@ CREATE DATABASE CV_DB;
 \connect cv_db;
 
 CREATE TABLE cv_table (
-      "cv_nr" serial,
+      "cv_nr" serial PRIMARY KEY,
       "username" text,
       "eng_introduction" text,
-      "fin_introduction" text,
-      PRIMARY KEY ("cv_nr")
+      "fin_introduction" text
 );
 
-CREATE TABLE cv_sections (
+CREATE TABLE sections (
       "cv_nr" serial,
       "section_nr" serial,
       "eng_body" text,
@@ -20,6 +19,34 @@ CREATE TABLE cv_sections (
       PRIMARY KEY ("cv_nr", "section_nr")
 );
 
+CREATE TABLE projects (
+      "cv_nr" serial,
+      "project_nr" serial,
+      "company_name" text,
+      "eng_title" text,
+      "fin_title" text,
+      PRIMARY KEY ("cv_nr", "project_nr")
+);
+
+CREATE TABLE employees (
+      "username" text PRIMARY KEY,
+      "name" text,
+      "photo" bytea,
+      "eng_title" text,
+      "fin_title" text
+);
+
+CREATE TABLE companies (
+      "company_name" text PRIMARY KEY,
+      "logo" bytea
+);
+
+CREATE TABLE section_headers (
+       "section_nr" serial PRIMARY KEY,
+       "eng_header" text,
+       "fin_header" text
+);
+
 INSERT INTO cv_table VALUES (1, 'user1', 'introduction1', 'esittely1');
 
-INSERT INTO cv_sections VALUES (1, 1, 'Some text in English', 'Suomenkielistä tekstiä');
+INSERT INTO sections VALUES (1, 1, 'Some text in English', 'Suomenkielistä tekstiä');

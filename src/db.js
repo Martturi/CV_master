e32a456c36a4a476d83cb90351ebfb7b57b0c513
samipusa <sami.pusa@gmail.com>
2017-12-11 17:51:47 +0200

    changed db.js to work with new tables

--- src/db.js
+++ src/db.js
@@ -4,18 +4,17 @@ const client = new Client({
   connectionString: process.env.DATABASE_URL,
 })
 
-let text = '';
-const username = 'user1';
+let text = ''
+const cv_nr = 1
+const section_nr = 1 //for testing
 
 client.connect()
 
-const selectQuery = 'SELECT eng_body FROM cv_sections WHERE cv_nr IN (SELECT cv_nr FROM cv_table WHERE username = $1);';
-
-client.query(selectQuery, [username], (err, result) => {
-  if (err) throw err;
+client.query('SELECT eng_body FROM sections WHERE cv_nr = $1 AND section_nr = $2;', [cv_nr, section_nr], (err, result) => {
+  if (err) throw err
   JSON.parse(JSON.stringify(result.rows[0]), (key, value) => {
     if (key === 'eng_body') {
-      text = value;
+      text = value
     }
   })
 })
@@ -23,16 +22,14 @@ client.query(selectQuery, [username], (err, result) => {
 const load = () => text
 
 const save = (input) => {
-  text = input;
-  const cv_nr = 1; //for testing purposes - later, cv_nr should be unique
-  const section_nr = 1; //for testing purposes - later, the combination of cv_nr and section_nr should be unique
-  const query = 'INSERT INTO cv_sections VALUES ($1, $2, $3, NULL) ON CONFLICT (cv_nr, section_nr) DO UPDATE SET eng_body = $3 WHERE cv_sections.cv_nr = $1;';
+  text = input
+  const query = 'UPDATE sections SET eng_body = $1 WHERE cv_nr = $2 AND section_nr = $3;'
   return new Promise((resolve, reject) => {
-    client.query(query, [cv_nr, section_nr, text], (err, result) => {
-      if (err) reject(err);
-      else if (result) resolve('Save succeeded.');
-    });
-  });
-};
+    client.query(query, [text, cv_nr, section_nr], (err, result) => {
+      if (err) reject(err)
+      else if (result) resolve('Save succeeded.')
+    })
+  })
+}
 
 module.exports = { load, save }

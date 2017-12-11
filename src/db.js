d879474d0e2c2a2f6d19ede647405324516bd307
samipusa <sami.pusa@gmail.com>
2017-12-11 17:51:47 +0200

    changed db.js to work with new tables

--- src/db.js
+++ src/db.js
@@ -5,14 +5,15 @@ const client = new Client({
 })
 
 let text = ''
-const id = 0
+const cv_nr = 1
+const section_nr = 1 //for testing
 
 client.connect()
 
-client.query('SELECT text FROM cv_table WHERE id = $1;', [id], (err, result) => {
+client.query('SELECT eng_body FROM sections WHERE cv_nr = $1 AND section_nr = $2;', [cv_nr, section_nr], (err, result) => {
   if (err) throw err
   JSON.parse(JSON.stringify(result.rows[0]), (key, value) => {
-    if (key === 'text') {
+    if (key === 'eng_body') {
       text = value
     }
   })
@@ -22,9 +23,9 @@ const load = () => text
 
 const save = (input) => {
   text = input
-  const query = 'UPDATE cv_table SET text = $1 WHERE id = $2;'
+  const query = 'UPDATE sections SET eng_body = $1 WHERE cv_nr = $2 AND section_nr = $3;'
   return new Promise((resolve, reject) => {
-    client.query(query, [text, id], (err, result) => {
+    client.query(query, [text, cv_nr, section_nr], (err, result) => {
       if (err) reject(err)
       else if (result) resolve('Save succeeded.')
     })

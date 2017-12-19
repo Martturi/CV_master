de76940a03e6921bc71530e1c5ef69a7119a44e8
Rasmus Järnström <rasmus.jarnstrom@aalto.fi>
2017-12-19 14:44:38 +0200

    Basic react app with opening and editing.
    First react app, nothing works.
    
    Edited eslint to add new file types
    
    React file structure. Builds in heroku.
    
    Updates cv from Database. Also fixes to esLint and dependencies.
    
    Run locally with "npm run watch", which runs both the server and client.
    Use at localhost:3000.
    
    Saves to database with hardcoded uid.
    
    Search bar and open-button works.
    
    Displays a save succeeded message for 3 seconds after success.

---
+++ react/src/components/CVEditor.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+class CVEditor extends Component {
+
+  handleChange(event) {
+    this.props.updateText(event.target.value)
+  }
+
+  render() {
+    return (
+      <div>
+        <textarea type="text" rows="10" cols="50" id="textfield" name="textfield" value={this.props.text} onChange={e => this.handleChange(e)} />
+        <div>
+          <button onClick={this.props.saveCV}>Save</button>
+        </div>
+        <div id="savestatus">{this.props.saveStatus}</div>
+      </div>
+    )
+  }
+}
+
+export default CVEditor

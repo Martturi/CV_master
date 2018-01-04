1269dc559efebeb7d9c20fdae18ac3f79a271c7f
Mia Savo <mia.savo@aalto.fi>
2018-01-04 15:42:17 +0200

    Fix to Travis/esLint.

--- react/src/components/Preview.js
+++ react/src/components/Preview.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { markdown } from 'markdown'
-
+/* eslint react/no-danger: 0 */
 const Preview = props =>
   <div dangerouslySetInnerHTML={{ __html: markdown.toHTML(props.text) }} />
 

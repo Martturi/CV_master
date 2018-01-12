168f6d0e59e424d003a99777e25908194867b621
annalonka <31215110+annalonka@users.noreply.github.com>
2018-01-12 16:04:41 +0200

    Created the browse layout without functionality.

---
+++ react/src/components/Browse/BrowseApp.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react'
+import BrowseButtonGroup from './BrowseButtonGroup'
+import NavBar from '../NavBar'
+import NameList from './NameList'
+import CVList from './CVList'
+import ExampleCV from '../ExampleCV.png'
+import './BrowseApp.css'
+import '../NavBar.css'
+
+
+class BrowseApp extends Component {
+
+  render() {
+    return (
+      <div>
+        <header id="navbar">
+          <NavBar />
+        </header>
+        <div id="buttons">
+          <BrowseButtonGroup />
+        </div>
+        <div id="namelist" className="browseSection">
+          <NameList />
+        </div>
+        <div className="lineContainer" id="lineContainer">
+          <div className="line" />
+        </div>
+        <div id="cvlist" className="browseSection">
+          <CVList />
+        </div>
+        <div className="CVpreview">
+          <img src={ExampleCV} height="726" width="533" alt="First page of an example CV" />
+        </div>
+        <div className="lineContainer">
+          <div className="line" />
+        </div>
+      </div>
+
+    )
+  }
+}
+
+
+export default BrowseApp
--- react/src/index.js
+++ react/src/index.js
@@ -1,8 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.css'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import App from './components/App'
+// import App from './components/App/App'
+// import EditorApp from './components/Editor/EditorApp'
+import BrowseApp from './components/Browse/BrowseApp'
+
 // import registerServiceWorker from './registerServiceWorker' //Caching is disabled for now.
 
-ReactDOM.render(<App />, document.getElementById('root'))
+// ReactDOM.render(<App />, document.getElementById('root'))
+// ReactDOM.render(<EditorApp />, document.getElementById('root'))
+ReactDOM.render(<BrowseApp />, document.getElementById('root'))
 // registerServiceWorker()

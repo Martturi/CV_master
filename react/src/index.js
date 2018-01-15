5c3a12786775d59f1ff555671de42a0d368816b8
Milla Alakuijala <milla.alakuijala@gmail.com>
2018-01-15 17:35:24 +0200

    Fixed Eslint errors

--- react/src/index.js
+++ react/src/index.js
@@ -3,12 +3,12 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 // import App from './components/App/App'
-import EditorApp from './components/Editor/EditorApp'
+// import EditorApp from './components/Editor/EditorApp'
 import BrowseApp from './components/Browse/BrowseApp'
 
 // import registerServiceWorker from './registerServiceWorker' //Caching is disabled for now.
 
 // ReactDOM.render(<App />, document.getElementById('root'))
-ReactDOM.render(<EditorApp />, document.getElementById('root'))
-//ReactDOM.render(<BrowseApp />, document.getElementById('root'))
+// ReactDOM.render(<EditorApp />, document.getElementById('root'))
+ReactDOM.render(<BrowseApp />, document.getElementById('root'))
 // registerServiceWorker()

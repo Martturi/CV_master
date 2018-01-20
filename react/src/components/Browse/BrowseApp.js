3961f395ee9577295310c4402ae7024a4c8d1817
Sami Pusa <pusas3@aalto.fi>
2018-01-20 16:32:15 +0200

    made BrowseButtonGroup stateless for simplicity

--- react/src/components/Browse/BrowseApp.js
+++ react/src/components/Browse/BrowseApp.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import BrowseButtonGroup from './BrowseButtonGroup'
 import NavBar from '../NavBar'
 import NameList from './NameList'
@@ -7,32 +7,46 @@ import ExampleCV from '../ExampleCV.png'
 import './BrowseApp.css'
 import '../NavBar.css'
 
-const BrowseApp = () => {
-  return (
-    <div>
-      <header id="navbar">
-        <NavBar />
-      </header>
-      <div id="buttons">
-        <BrowseButtonGroup />
+class BrowseApp extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { exportDropDownOpen: false }
+  }
+
+  toggleExportButton() {
+    this.setState({ exportDropDownOpen: !this.state.exportDropDownOpen })
+  }
+
+  render() {
+    return (
+      <div>
+        <header id="navbar">
+          <NavBar />
+        </header>
+        <div id="buttons">
+          <BrowseButtonGroup
+            dropdownOpen={this.state.exportDropDownOpen}
+            toggle={() => this.toggleExportButton()}
+          />
+        </div>
+        <div id="namelist" className="browseSection">
+          <NameList />
+        </div>
+        <div className="lineContainer" id="lineContainer">
+          <div className="line" />
+        </div>
+        <div id="cvlist" className="browseSection">
+          <CVList />
+        </div>
+        <div className="CVpreview">
+          <img src={ExampleCV} height="726" width="533" alt="First page of an example CV" />
+        </div>
+        <div className="lineContainer">
+          <div className="line" />
+        </div>
       </div>
-      <div id="namelist" className="browseSection">
-        <NameList />
-      </div>
-      <div className="lineContainer" id="lineContainer">
-        <div className="line" />
-      </div>
-      <div id="cvlist" className="browseSection">
-        <CVList />
-      </div>
-      <div className="CVpreview">
-        <img src={ExampleCV} height="726" width="533" alt="First page of an example CV" />
-      </div>
-      <div className="lineContainer">
-        <div className="line" />
-      </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default BrowseApp
--- react/src/components/Browse/BrowseButtonGroup.js
+++ react/src/components/Browse/BrowseButtonGroup.js
@@ -1,58 +1,39 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { ButtonDropdown, DropdownToggle, DropdownMenu, DropdownItem, ButtonGroup, Button, Input } from 'reactstrap'
 
 
-class BrowseButtonGroup extends Component {
-  constructor(props) {
-    super(props)
-    this.toggle = this.toggle.bind(this)
-    this.state = {
-      dropdownOpen: false,
-    }
-  }
-
-  toggle() {
-    this.setState({
-      dropdownOpen: !this.state.dropdownOpen,
-    })
-  }
-
-
-  render() {
-    return (
-      <div className="buttonheader">
-        <Input className="search" placeholder="Search..." />
+const BrowseButtonGroup = (props) => {
+  return (
+    <div className="buttonheader">
+      <Input className="search" placeholder="Search..." />
+      <Button outline className="button">
+        <span className="fa fa-search" aria-hidden="true" />
+      </Button>
+      <ButtonGroup className="exportgroup">
         <Button outline className="button">
-          <span className="fa fa-search" aria-hidden="true" />
-        </Button>
-        <ButtonGroup className="exportgroup">
-          <Button outline className="button">
             Rename
-          </Button>
-          <Button outline className="button">
-            <span className="fa fa-pencil" aria-hidden="true" />
-          </Button>
-          <Button outline className="button">
-            <span className="fa fa-files-o" aria-hidden="true" />
-          </Button>
-          <Button outline className="button">
-            <span className="fa fa-trash-o" aria-hidden="true" />
-          </Button>
-          <ButtonDropdown isOpen={this.state.dropdownOpen} toggle={this.toggle}>
-            <DropdownToggle caret outline className="button">
+        </Button>
+        <Button outline className="button">
+          <span className="fa fa-pencil" aria-hidden="true" />
+        </Button>
+        <Button outline className="button">
+          <span className="fa fa-files-o" aria-hidden="true" />
+        </Button>
+        <Button outline className="button">
+          <span className="fa fa-trash-o" aria-hidden="true" />
+        </Button>
+        <ButtonDropdown isOpen={props.dropdownOpen} toggle={props.toggle}>
+          <DropdownToggle caret outline className="button">
               Export
-            </DropdownToggle>
-            <DropdownMenu>
-              <DropdownItem>Download as PDF</DropdownItem>
-              <DropdownItem>Share via email</DropdownItem>
-            </DropdownMenu>
-          </ButtonDropdown>
-        </ButtonGroup>
-      </div>
-
-
-    )
-  }
+          </DropdownToggle>
+          <DropdownMenu>
+            <DropdownItem>Download as PDF</DropdownItem>
+            <DropdownItem>Share via email</DropdownItem>
+          </DropdownMenu>
+        </ButtonDropdown>
+      </ButtonGroup>
+    </div>
+  )
 }
 
 

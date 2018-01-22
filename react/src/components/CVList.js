03fdd08944c0d7112a7fc5aafef3e675b4cbc1f8
samipusa <sami.pusa@gmail.com>
2018-01-22 23:43:38 +0200

    Combined user and cv list functionality

--- react/src/components/CVList.js
+++ react/src/components/CVList.js
@@ -1,34 +1,11 @@
 import React from 'react'
 import { ListGroup, ListGroupItem, ListGroupItemText, ListGroupItemHeading } from 'reactstrap'
 import CVToolbar from './CVToolbar'
-import { loadCVList } from './Api'
 
 class CVList extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      cvList: [],
-      selectedIndex: 0,
-    }
-  }
-
-  componentDidMount() {
-    this.updateCVList()
-    this.render()
-  }
-
-  updateCVList() {
-    const testUsername = 'Heikki Heikalainen'
-    loadCVList(testUsername)
-      .then((cvs) => {
-        this.setState({ cvList: cvs })
-      })
-      .catch(err => console.log(err))
-  }
-
   render() {
-    const listGroupItems = this.state.cvList.map((cvName, index) => {
-      const isActive = this.state.selectedIndex === index
+    const listGroupItems = this.props.cvList.map((cvName, index) => {
+      const isActive = this.props.selectedCVIndex === index
       return (
         <ListGroupItem active={isActive}>
           <div className="cvinfo">

15c3a22dc00b9c6568cdd14e3ae35e1e89221b83
Rasmus Järnström <rasmus.jarnstrom@aalto.fi>
2018-01-31 20:31:52 +0200

    PDF renders header and footer on each page.

--- server/pdf.js
+++ server/pdf.js
@@ -4,21 +4,32 @@ const fs = require('fs')
 const path = require('path')
 const ejs = require('ejs')
 
+const header = fs.readFileSync(path.resolve(__dirname, './pdf/header.html'), 'utf-8')
+const footer = fs.readFileSync(path.resolve(__dirname, './pdf/footer.html'), 'utf-8')
 const options = {
   base: `file://${__dirname}/../react/public/`,
+  header: {
+    height: '20mm',
+    contents: header,
+  },
+  footer: {
+    height: '12mm',
+    contents: {
+      default: footer,
+    },
+  },
 }
 
 const getHTML = (text) => {
-  const style = fs.readFileSync(path.resolve(__dirname, './pdf/pdf.css'), 'utf-8')
+  const style = fs.readFileSync(path.resolve(__dirname, 'pdf/pdf.css'), 'utf-8')
   const template = fs.readFileSync(path.resolve(__dirname, 'pdf/preview.ejs'), 'utf-8')
   const parsedHTML = markdown.toHTML(text)
   return ejs.render(template, { styles: style, text: parsedHTML })
 }
 
 const servePDF = (text, response) => {
-  console.log(`Creating pdf ${text}`)
+  console.log(`Creating pdf ${text.substring(0, 100)}`)
   const parsedHTML = getHTML(text)
-  console.log(parsedHTML)
   pdf.create(parsedHTML, options).toStream((err, stream) => {
     response.setHeader('Content-Type', 'application/pdf')
     response.setHeader('Content-Disposition', 'attachment; filename=cv.pdf')
@@ -30,6 +41,5 @@ const servePDF = (text, response) => {
 module.exports = { getHTML, servePDF }
 
 /*
-FIXME header and footer only show on first and last page, respectively.
 FIXME page breaks on PDF
  */

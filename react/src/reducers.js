3a025a0251a57483ba18b34f05f14476300e2981
martturi <martturi@gmail.com>
2018-02-19 12:29:31 +0200

    Initial Redux integration

---
+++ react/src/reducers.js
@@ -0,0 +1,13 @@
+const initialState = {
+    selectedUser: '',
+    selectedCV: '',
+}
+
+const CVreducer = (state = initialState, action) => {
+    switch (action.type) {
+        default:
+            return state
+    }
+}
+
+export default CVreducer

3697645080be57f5de9b6899317d39a1b9744752
Mia Savo <mia.savo@aalto.fi>
2018-03-06 16:04:57 +0200

    Redirect to a 404 page from invalid URL.

---
+++ react/src/components/NotFound.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const NotFound = () => {
+  return (
+    <div>
+      <h3>404 page not found</h3>
+      <p>URL entered was invalid, please try again.</p>
+      <a href="http://cv-master.herokuapp.com">Home page</a>
+    </div>
+  )
+}
+
+export default NotFound

7cfe2f3496c7c548a641b0c9e310095bbd358a71
Milla Alakuijala <milla.alakuijala@gmail.com>
2018-03-30 11:50:51 +0300

    Change downloaded PDF name to contain full name

--- react/src/utils.js
+++ react/src/utils.js
@@ -2,11 +2,15 @@ import Api from './Api'
 
 async function downloadPDF(username, cvID, sections) {
   const res = await Api.fetchPDF(username, sections)
+  const users = await Api.loadUserList()
+  const fullName = users.find(u => u.username === username).full_name.toString()
+  const firstName = fullName.split(' ')[0]
+  const lastName = fullName.split(' ')[1]
   const blob = await res.blob()
-  const file = new File([blob], `${username}_${cvID}.pdf`, { type: 'application/pdf' })
+  const file = new File([blob], `${firstName}_${lastName}_${cvID}.pdf`, { type: 'application/pdf' })
   const a = document.createElement('a')
   a.href = URL.createObjectURL(file)
-  a.download = `${username}_${cvID}.pdf`
+  a.download = `${firstName}_${lastName}_${cvID}.pdf`
   document.body.appendChild(a)
   a.click()
   document.body.removeChild(a)
